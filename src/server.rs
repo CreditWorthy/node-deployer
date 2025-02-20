8285058202f3b1c351431c0eca78f748555eb0b3
Alexzander Summerville <alexzandersummerville@gmail.com>
2025-02-19 20:25:47 -0600

    cors layer

--- src/server.rs
+++ src/server.rs
@@ -10,7 +10,8 @@ use axum::{
 };
 use rstar::RTree;
 use serde::{Deserialize, Serialize};
-use tower_http::services::{ServeDir, ServeFile};
+use tower::ServiceBuilder;
+use tower_http::{cors::{Any, CorsLayer}, services::{ServeDir, ServeFile}};
 
 use crate::{
     engine::Engine,
@@ -22,151 +23,25 @@ struct AppState {
     engine: Engine,
 }
 
-#[test]
-fn test_rc() {
-    struct S {
-        a: f64,
-    };
-
-    //  impl !Send for S {}
-
-    // impl Drop for S {
-    //     fn drop(&mut self) {
-    //         println!("dropping S");
-    //     }
-    // }
-
-    // {
-    //     let s = S{a: 111.0}; // on stack
-    //     let mul_owned = Arc::new(s); // T (move by default unless T is copy) vs. &T
-
-    //     // let b = Box::new(s); // allocate `s` on the heap instead of original statck.
-
-    //     // b is on stack. but underlying `s` is on the heap.
-    //     // Box / String: owned pointer (in some sense)
-
-    //     // Deref trait: smart pointer in rust
-    //     // deref(&self) -> &T:
-    //     // mul_owned.a; // mul_owned.deref().a; automically by compiler
-
-    //     // let x = Rc::clone(&mul_owned); // only clone pointer Rc
-    //     // let y = mul_owned.clone(); // only clone pointer Rc
-
-    //     // rust prevet misuse of Rc through the Send trait.
-    //     // Sync trait: safe borrow to some values in multiple threads.
-    //     // &T: Send == T: Sync.
-    //     // `S` is Send
-
-    //     let mul_owned2 = Arc::clone(&mul_owned);
-
-    //     //  Arc: immutable share
-    //     // Mutex. can give mutable share to its wrapped inner value.
-
-    //     // DerefMut.
-    //     let mx = std::sync::Mutex::new(S{a:2.0});
-
-    //     std::thread::spawn(move || {
-    //         // child access variable owned by parent: danling pointer
-    //         println!("{}", mul_owned.a);
-    //         mx.lock().unwrap().a = 2.0;
-    //     });
-
-    //     std::thread::spawn(move || {
-    //         // child access variable owned by parent: danling pointer
-    //         println!("{}", mul_owned2.a);
-    //     });
-
-    //     // Rc: not multi-thread safe. how compiler ensure programms not mis-use it in multi-thread env?
-    //     // Send & Sync.
-    //     // Arc: it's safe version of Rc
-    // }
-
-    println!("== s scope finish");
-}
-
 pub async fn server_start(address: &str, map_path: &str) {
     let engine = Engine::build(map_path).unwrap();
     let shared_state = Arc::new(AppState { engine });
 
+    let cors = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_origin(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/nav", get(nav))
+        .layer(ServiceBuilder::new().layer(cors))
         // put web/index.html in the same dir of our final binary
         // in docker:
         //    /app (dir)
         //    /app/simple-nav (binary)
         //    /app/web/index.html (static web page)
-        .nest_service(
-            "/static",
-            ServeDir::new("web").not_found_service(ServeFile::new("web/index.html")),
-        )
         .with_state(shared_state);
-
-    // 1. ownership system: (RAII) manage memory/resources semi-auto (semi-gc).
-    // 2. borrow & references:
-    // 3. multiple ownership problem: (only semantic/conceptually) , rust used some special types : Rc / Arc
-
-    // reasoning like:
-    // 1. use Mutex to protece shared data in multiple threads
-    // 2. and use Mutex::lock() (returns MutexGuard) to aquire lock
-    // 3. Drop for MutexGuard to release lock automatically
-    // 4. Problem: MutexGuard can not send to another threads.
-
-    // Send: move / transfer ownership to other threads
-
-    // CANNOT DO:
-    // have some shared object (shared_state)
-    // Mutex wrap (shared_state)
-    // MutextGuard cannot be send to multiple threads to access
-
-    // let mutex_shared = std::sync::Mutex::new(String::from("hello")); // not 'static variable
-
-    // *guard = String::from("world");
-    // !Send meaning
-
-    // compiler will derive a FnOnce trait implementation for closure here auto.
-    // Fn/FnMut (compiler built-in trait)
-    // ~~Marker traits examples.~~
-
-    // spawn a child thread in main thread. child thread can run longer the main thread.
-    // spawn expects a static lifetime closure parameter
-    // std::thread::spawn(move || {
-    //     // so if you access some value inside a child thread, memory issue: resource realsed by main thread.
-    //     // time sleep 1 hours, mutex_shared destroied/reclaimed because finish of main thread.
-    //     // dangling reference/memory
-
-    //     // 'static constraints means closure either owns used variables/values or only refer/borrow some 'static variables/values
-    //     let mut guard = mutex_shared.lock().unwrap();
-    //     *guard = String::from("world");
-    // });
-
-    // Mutex vs Arc.
-    // Arc: atomic reference count , 1. multiple ownerships, 2. thread-safe
-    // 1. multiple ownerships (conceptually, not syntax lly): e.g. share part of lists
-    //     a -> b -> c
-    //          ^
-    //     d -> e
-    //  a and e both owns (b -> c) child list
-    //  when releasing list start with `a`, you cannot just release (b -> c) child list.
-    //   `Rc` for single thread, `Arc for multi-thread.
-
-    //  Rc<Node>: b
-    //  a.
-
-    //
-
-    // Drop trait it's called by compiler when out of scope (RAII):
-    // (MutexGuard): it will release the corresponding lock automatically.
-
-    // u64, is Send.
-    // lock is usually not safe to send. POSIX
-    // lock is not implemented Send trait
-
-    // Send & Sync marker traits (auto trait)
-    // Send: express if value of some type can be sent to another thread
-    // Sync: express if reference of some type can be sent to another thread.
-    // by default compiler will derive these two traits for your defined types,
-    // if you want implements Send and Sync you need to use unsafe.
 
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
